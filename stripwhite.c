0bc2bbd8083d7ff9be908dfce51101a6c28eedff
Stefan Peng <stefanpeng9@gmail.com>
2016-08-20 04:45:44 +0000

    1-18

--- stripwhite.c
+++ stripwhite.c
@@ -1,3 +1,58 @@
 #include <stdio.h>
+#define MAXLINE 1000	/* maximum input line size */
 
 /* 1-18 strip trailing white space, empty lines */
+int getln(char line[], int maxline);
+void stripwhite(char line[], int len);
+
+int main()
+{
+    char line[MAXLINE];	/* current input line */
+    int len = 0;
+    int i = 0;
+
+    len = getln(line, MAXLINE);
+    
+    stripwhite(line, len);
+    printf("%s", line);
+	return 0;
+}
+
+/* getln: read a line into s, return length */
+int getln(char s[], int lim)
+{
+	int c, i;
+
+	for (i=0; i<lim-1 && (c=getchar()) !=EOF && c!='\n'; ++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+/* stripwhite: remove trailing whitespace, delete entirely blank lines */
+void stripwhite(char line[], int len) {
+    int i = 0;
+    int l = len;
+    
+    /* delete blank lines */
+    if (line[0]=='\n') {
+        line[0] = '\0';
+    }
+    
+    /* strip trailing whitespace */
+    for (i=len-2; i>=0; i--) {
+        if (line[i]==' ' || line[i]=='\t') {
+            continue;
+        } else {
+            l = i;
+            break;
+        }
+    }
+    
+    line[l+1] = '\n';
+    line[l+2] = '\0';
+}
